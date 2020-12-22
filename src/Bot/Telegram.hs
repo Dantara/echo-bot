31444ea0be7c457a7b334786b431ec4478d7a572
Dantara <supercreed2@gmail.com>
2020-12-23 01:42:17 +0300

    Basic parsing

--- src/Bot/Telegram.hs
+++ src/Bot/Telegram.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 module Bot.Telegram where
 
 import           Bot
+import           Control.Monad.IO.Class
 import           Control.Monad.Reader
+import           Data.Aeson
+import           Data.Aeson.Types
 import           Logger
+import           Message
+import           Network.HTTP.Req
 
 
 newtype TelegramBot x = TelegramBot (ReaderT BotEnv IO x)
@@ -23,3 +29,42 @@ instance MonadBot TelegramBot where
 
 instance Logger TelegramBot where
   log = logSTD
+
+
+pullUpdates :: IO ()
+pullUpdates = do
+  let payload =
+        object
+          [ "offset" .= (0 :: Int)
+          ]
+  r <- runReq defaultHttpConfig
+    $ req
+        POST
+        (https "api.telegram.org" /: token' /: "getUpdates")
+        (ReqBodyJson payload)
+        jsonResponse
+        mempty
+  liftIO $ print (responseBody r :: Updates)
+    where
+      token' = "bot" <> "1470862909:AAGZF-lhbKci7azP-NHsxiTCdGJ4flHlTDo"
+
+
+data Update = Update
+  { updateId :: Integer
+  , message  :: Message
+  } deriving Show
+
+
+instance FromJSON Update where
+  parseJSON = withObject "Update" $ \upd -> Update
+    <$> upd .: "update_id"
+    <*> upd .: "message"
+
+
+newtype Updates = Updates [Update]
+  deriving Show
+
+
+instance FromJSON Updates where
+  parseJSON = withObject "Updates" $ \us -> Updates
+    <$> us .: "result"
--- src/Message.hs
+++ src/Message.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Message where
 
+import           Data.Aeson
+import           Data.Text  (Text)
+
+
 data Message = Message
+  { chatId :: Integer
+  , text   :: Maybe Text
+  } deriving Show
+
+
+instance FromJSON Message where
+  parseJSON = withObject "Message" $ \msg -> Message
+    <$> (msg .: "chat" >>= (.: "id"))
+    <*> msg .:? "text"
